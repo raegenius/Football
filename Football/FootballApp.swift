1fb3693cef664e3378989fcf8d12e9e6a27317e2
Raemur <raemur@gmail.com>
2025-05-27 22:06:33 -0700

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Raemur Bedward on 2025-05-27.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
